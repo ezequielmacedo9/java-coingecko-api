65f60e2a5f1872d6ea060fb84d38b98183e3ba28
Ezequiel <ezequielmacedo444@gmail.com>
2025-12-27 15:14:03 -0300

    Add files via upload

---
+++ PrincipalApi.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class PrincipalApi {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("\nDigite ID da Criptomoeda: \n");
+        var busca = leitura.nextLine();
+        String buscaCodificada = URLEncoder.encode(busca, StandardCharsets.UTF_8);
+
+        String endereco = "https://api.coingecko.com/api/v3/simple/price?ids=" + buscaCodificada + "&vs_currencies=usd";
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+
+
+    }
+}
